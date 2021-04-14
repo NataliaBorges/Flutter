31227fffb9e31e16fcfe27fef9aac1101105d778
Natalia_Borges <Nataliaborgessilva1@gmail.com>
2021-04-13 22:36:08 -0300

    Usando classe em Dart

---
+++ Classe.dart
@@ -0,0 +1,34 @@
+class Pessoa{
+  String nome;
+  int idade;
+  double altura;
+  
+  void dormir(){
+    print("$nome está dormindo!");
+  }
+  void aniver(){
+    idade++;
+  }
+}
+
+void main() {
+  Pessoa pessoa1 = Pessoa();
+  pessoa1.nome = "João";
+  pessoa1.idade = 30;
+  pessoa1.altura = 1.80;
+  
+  Pessoa pessoa2 = Pessoa();
+  pessoa2.nome = "Thiago";
+  pessoa2.idade = 28;
+  pessoa2.altura = 1.90;
+  
+  print(pessoa1.nome);
+  print(pessoa2.nome);
+  
+  print(pessoa1.idade);
+  pessoa1.aniver();
+  print(pessoa1.idade);
+  
+  pessoa2.dormir();
+  
+}
